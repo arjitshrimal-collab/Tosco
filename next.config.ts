cac594ffe376adfcaa7c5cb3e25b7e55eed2cc83
Arjit <arjitshrimal@gmail.com>
2026-05-03 21:44:45 -0400

    Fix Prisma client

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    dynamicIO: false,
-  },
-  output: 'standalone',
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
